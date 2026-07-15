a440d2fb2a9e1dce3e620ac1e0736e67be51e21b
malo <malo.gabriel.2@gmail.com>
2026-07-15 15:32:21 -0400

    Refactored main.c and added --veromikka flag

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc > 1) {
-        if (strcmp(argv[1], "--help") == 0) {
-            printf("Didn't provide help.\n");
-        } else if (strcmp(argv[1], "--sandbox") == 0) {
-            printf("Didn't sandboxed doas.\n");
-        }
+typedef struct {
+    const char *flag;
+    const char *message;
+} Flag;
 
-        return 0;
+int main(int argc, char *argv[]) {
+    const Flag flags[] = {
+        {"--help", "Didn't provide help."},
+        {"--sandbox", "Didn't sandboxed doas."},
+        {"--veromikka", "sowwy, couldn't dwoas :c"},
+    };
+    const int flags_len = sizeof(flags) / sizeof(flags[0]);
+
+    if (argc > 1) {
+        for (int i = 0; i < flags_len; i++) {
+            if (strcmp(argv[1], flags[i].flag) == 0) {
+                printf("%s\n", flags[i].message);
+                return 0;
+            }
+        }
     }
 
     printf("Didn't doas.\n");
