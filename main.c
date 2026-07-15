3befa9c4a539723bf65ef4e982859c93b6409a88
ave <av3_e@proton.me>
2026-07-15 06:08:06 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    printf("Didn't doas.\n");
+int main(int argc, char *argv[]) {
+    if (argc > 1 && strcmp(argv[1], "--sandbox") == 0) {
+        printf("Didn't sandboxed doas.\n");
+    } else {
+        printf("Didn't doas.\n");
+    }
+
     return 0;
 }
